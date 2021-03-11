79212a2c063ccf09bfedf1a74a04281f6ed2be87
ThiagoLRG <54580798+ThiagoLRG@users.noreply.github.com>
2021-03-11 08:03:09 -0300

    with the correction

--- MCVE.py
+++ MCVE.py
@@ -10,9 +10,23 @@ def sorec(dictrecorrencias):
     print(f'len list after sorec changes when the program is restarted: {len(listarecorrenciaspronta)}')
     return listarecorrenciaspronta
 
+def sorec_corrected(dictrecorrencias):
+    listarecorrenciaspronta = []
+    for chave, valor in dictrecorrencias.items():
+        if len(valor) > 1:
+            setv = {v[0:3] for v in valor}
+            if sorted(tuple(setv)) == sorted(chave[1]):
+                listarecorrenciaspronta.append((chave[0], valor))
+    print(f'with sorted stays the same: {len(listarecorrenciaspronta)}')
+    return listarecorrenciaspronta
+
+
 with open(r'segmentacao.p', 'rb') as f:
     segmentacao = pickle.loads(f.read())
 
-segmentacao1 = sorec(segmentacao)
-segmentacao2 = sorec(segmentacao)
-segmentacao3 = sorec(segmentacao)
+sorec(segmentacao)
+sorec(segmentacao)
+sorec(segmentacao)
+sorec_corrected(segmentacao)
+sorec_corrected(segmentacao)
+sorec_corrected(segmentacao)
